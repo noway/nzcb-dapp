13815190952fed1ea19b04201d98280b661e930b
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-03-02 21:34:14 +1300

    add proof generation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { getToBeSigned } from "./nzcp";
 import wc from "./witness_calculator.js";
+import {groth16} from 'snarkjs'
 
 const EXAMPLE_PASS_URI = "NZCP:/1/2KCEVIQEIVVWK6JNGEASNICZAEP2KALYDZSGSZB2O5SWEOTOPJRXALTDN53GSZBRHEXGQZLBNR2GQLTOPICRUYMBTIFAIGTUKBAAUYTWMOSGQQDDN5XHIZLYOSBHQJTIOR2HA4Z2F4XXO53XFZ3TGLTPOJTS6MRQGE4C6Y3SMVSGK3TUNFQWY4ZPOYYXQKTIOR2HA4Z2F4XW46TDOAXGG33WNFSDCOJONBSWC3DUNAXG46RPMNXW45DFPB2HGL3WGFTXMZLSONUW63TFGEXDALRQMR2HS4DFQJ2FMZLSNFTGSYLCNRSUG4TFMRSW45DJMFWG6UDVMJWGSY2DN53GSZCQMFZXG4LDOJSWIZLOORUWC3CTOVRGUZLDOSRWSZ3JOZSW4TTBNVSWISTBMNVWUZTBNVUWY6KOMFWWKZ2TOBQXE4TPO5RWI33CNIYTSNRQFUYDILJRGYDVAYFE6VGU4MCDGK7DHLLYWHVPUS2YIDJOA6Y524TD3AZRM263WTY2BE4DPKIF27WKF3UDNNVSVWRDYIYVJ65IRJJJ6Z25M2DO4YZLBHWFQGVQR5ZLIWEQJOZTS3IQ7JTNCFDX"
 
@@ -28,16 +29,6 @@ function bufferToBitArray(b: Uint8Array) {
 
 function App() {
   const [passURI, setPassURI] = useState(EXAMPLE_PASS_URI);
-  const witnessCalculator = useRef<null | any>(null)
-  useEffect(() => {
-    async function load() {
-      const res = await fetch("nzcp_example.wasm")
-      const buffer = await res.arrayBuffer();
-      witnessCalculator.current = await wc(buffer)
-    }
-    load()
-
-  }, [])
   const verify = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (passURI.length > 0) {
@@ -45,10 +36,18 @@ function App() {
       const ToBeSigned = getToBeSigned(passURI)
       const data = prepareToBeSigned(ToBeSigned, 314)
       const input = { toBeSigned: bufferToBitArray(data.bytes), toBeSignedLen: data.bytesLen }
-      const buff = await witnessCalculator.current.calculateWTNSBin(input, 0);
+      console.log('proving...', input)
+      try {
+        const { proof, publicSignals } = await groth16.fullProve(input, "nzcp_example.wasm", "nzcp_example_0001.zkey")
+        console.log('proof', proof, publicSignals)
+      }
+      catch (e) {
+        console.log('proof error', e)
+      }
+      // const buff = await witnessCalculator.current.calculateWTNSBin(input, 0);
 
       // alert(JSON.stringify(input, null, 2));
-      console.log(buff)
+      // console.log(buff)
     }
   };
   return (
