32bd4421a082b197fc0da54831d7605c73c724cb
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-03-05 15:06:06 +1300

    wip circuit result verifier - doesn't match at the moment

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import { decodeBytes, decodeCOSE, encodeToBeSigned } from "./nzcpTools";
 import { groth16 } from 'snarkjs'
-import { bufferToBitArray } from "./utils";
+import { bitArrayToBuffer, bufferToBitArray, compare } from "./utils";
 import { getNZCPPubIdentity, fitBytes } from "./nzcpCircom";
 
 const EXAMPLE_PASS_URI = "NZCP:/1/2KCEVIQEIVVWK6JNGEASNICZAEP2KALYDZSGSZB2O5SWEOTOPJRXALTDN53GSZBRHEXGQZLBNR2GQLTOPICRUYMBTIFAIGTUKBAAUYTWMOSGQQDDN5XHIZLYOSBHQJTIOR2HA4Z2F4XXO53XFZ3TGLTPOJTS6MRQGE4C6Y3SMVSGK3TUNFQWY4ZPOYYXQKTIOR2HA4Z2F4XW46TDOAXGG33WNFSDCOJONBSWC3DUNAXG46RPMNXW45DFPB2HGL3WGFTXMZLSONUW63TFGEXDALRQMR2HS4DFQJ2FMZLSNFTGSYLCNRSUG4TFMRSW45DJMFWG6UDVMJWGSY2DN53GSZCQMFZXG4LDOJSWIZLOORUWC3CTOVRGUZLDOSRWSZ3JOZSW4TTBNVSWISTBMNVWUZTBNVUWY6KOMFWWKZ2TOBQXE4TPO5RWI33CNIYTSNRQFUYDILJRGYDVAYFE6VGU4MCDGK7DHLLYWHVPUS2YIDJOA6Y524TD3AZRM263WTY2BE4DPKIF27WKF3UDNNVSVWRDYIYVJ65IRJJJ6Z25M2DO4YZLBHWFQGVQR5ZLIWEQJOZTS3IQ7JTNCFDX"
@@ -12,6 +12,7 @@ function App() {
   const [passURI, setPassURI] = useState(EXAMPLE_PASS_URI);
   const [proving, setProving] = useState(false);
   const [error, setError] = useState<Error | null>(null);
+  const [circuitResultMatches, setCircuitResultMatches] = useState<boolean>(false);
 
   const prove = async (passURI: string) => {
     const bytes = decodeBytes(passURI);
@@ -27,6 +28,16 @@ function App() {
     setProving(true)
     try {
       const { proof, publicSignals } = await groth16.fullProve(input, "nzcp_example.wasm", "nzcp_example_0001.zkey")
+
+      const credSubjHash = bitArrayToBuffer(publicSignals.slice(0, 256));
+      const toBeSignedHash = bitArrayToBuffer(publicSignals.slice(256, 512));
+      const exp = Number(publicSignals[512]);
+
+      if (compare(credSubjHash, pubIdentity.credSubjHash) && compare(toBeSignedHash, pubIdentity.toBeSignedHash) && exp === pubIdentity.exp) {
+        setCircuitResultMatches(true)
+      } else {
+        setCircuitResultMatches(false)
+      }
       console.log('proof', proof, publicSignals)
     }
     catch (e) {
@@ -48,6 +59,7 @@ function App() {
       <header className="App-header">
         {proving ? "Proving, this may take a while..." : ""}
         {error ? "Error while proving:  " + error.message : ""}
+        <p>Circuit result matches: {circuitResultMatches ? "yes" : "no"}</p>
         <form onSubmit={verify}>
           <input
             type="text"
--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,21 @@ export function bufferToBitArray(b: Uint8Array) {
   }
   return res;
 }
+
+export function bitArrayToBuffer(a: number[]) {
+  const len = Math.floor((a.length - 1) / 8) + 1;
+  const b = new Uint8Array(len);
+
+  for (let i = 0; i < a.length; i++) {
+      const p = Math.floor(i / 8);
+      b[p] = b[p] | (Number(a[i]) << (7 - (i % 8)));
+  }
+  return b;
+}
+
+export function compare(a: Uint8Array, b: Uint8Array) {
+  for (let i = a.length; -1 < i; i -= 1) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
