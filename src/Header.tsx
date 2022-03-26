0e7d19aae1f6b81fdd48e07d80adca9445bebd5f
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-03-26 17:21:40 +1300

    add connect button

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,24 @@
+import { styled } from "./styles";
+
+
+export const Navigation = styled("div", {
+  display: "flex",
+  justifyContent: "space-between"
+});
+
+
 export function Header() {
+  function back() {
+  }
+
+  async function connect() {
+  }
   return (
     <header>
+      <Navigation>
+        <button type="button" onClick={back}>Back</button>
+        <button type="button" onClick={connect}>Connect Wallet</button>
+      </Navigation>
       <h1>NZ COVID Badge</h1>
     </header>
   );
