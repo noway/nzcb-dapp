a76be64f8bb3640ae798be2f6a8f774a541219d5
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-03-26 15:17:21 +1300

    use globalStyles instead of index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import { globalStyles } from './styles';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +15,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+globalStyles()
--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,6 @@
 import { createStitches } from "@stitches/react";
 
-export const {styled, css} = createStitches({
+export const {styled, css, globalCss} = createStitches({
   theme: {
     colors: {
       gray400: 'gainsboro',
@@ -8,3 +8,12 @@ export const {styled, css} = createStitches({
     },
   },
 })
+
+export const globalStyles = globalCss({
+  body: {
+    margin: 0,
+    fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+    '-webkit-font-smoothing': 'antialiased',
+    '-moz-osx-font-smoothing': 'grayscale',
+  },
+});
