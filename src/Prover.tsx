addf10437fef236597c3a10780427ec0b1320bab
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-04-02 16:25:24 +1300

    pass info in a new file and reuse

--- src/Prover.tsx
+++ src/Prover.tsx
@@ -4,43 +4,8 @@ import { getNZCPPubIdentity, getNZCPCircuitInput, PubIdentity, Proof, PublicSign
 import { RouteContext } from "./contexts";
 import { EXAMPLE_WASM_FILE, EXAMPLE_ZKEY_FILE } from "./config";
 import { CtaContainer } from "./styles";
-import { DataSection } from "./DataSection";
-import { DataBit } from "./DataBit";
-import { ctiToJti, Data, decodeAlg, decodeBytes, decodeCBOR, decodeCOSE } from "./nzcpTools";
+import { PassInfo } from "./PassInfo";
 
-
-function PassInfo(props: Readonly<{ passURI: string }>) {
-  const { passURI } = props
-  const bytes = decodeBytes(passURI);
-  const cose = decodeCOSE(bytes);
-  const claims = decodeCBOR(cose.payload) as Map<Data, Data>;
-  const headers = decodeCBOR(cose.bodyProtected) as Map<Data, Data>;
-  const kid = headers.get(4) as Uint8Array
-  const alg = headers.get(1) as number
-  const iss = claims.get(1) as string;
-  const nbf = BigInt(claims.get(5) as number);
-  const exp = BigInt(claims.get(4) as number);
-  const cti = claims.get(7) as Uint8Array;
-  const vc = claims.get("vc") as Map<string, Data>;
-  const credentialSubject = vc.get("credentialSubject") as Map<string, string>;
-  const givenName = credentialSubject.get("givenName");
-  const familyName = credentialSubject.get("familyName");
-  const dob = credentialSubject.get("dob");
-
-  return (
-    <DataSection title="Pass info">
-      <DataBit title="kid" value={`${new TextDecoder("utf-8").decode(kid)}`} />
-      <DataBit title="alg" value={`${decodeAlg(alg)}`} />
-      <DataBit title="iss" value={`${iss}`} />
-      <DataBit title="nbf" value={`${nbf}`} />
-      <DataBit title="exp" value={`${exp}`} />
-      <DataBit title="jti" value={`${ctiToJti(cti)}`} />
-      <DataBit title="givenName" value={`${givenName}`} />
-      <DataBit title="familyName" value={`${familyName}`} />
-      <DataBit title="dob" value={`${dob}`} />
-    </DataSection>
-  )
-}
 
 type Props = Readonly<{
   passURI: string
