85ef9186ce47fe3820d7a1ed34a93eb82c59a3be
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-04-12 22:46:05 +1200

    add live config vars
    live to be signed max
    
    whitespace

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,27 @@
 export const CONTRACT_ADDRESS = "0xBED1aCf0708723f8b9F6632571f5F79866DD95b1";
-export const EXAMPLE_TOBESIGNED_MAX = 314;
+export const USE_REAL_PROOF = false;
+export const NETWORK = "rinkeby";
 
-export const EXAMPLE_ASSET_URL = "https://cloudflare-ipfs.com/ipfs/QmXKiU24bL3t86aJNvFTGSbXdSA8CUFJM223MdaKqG9LfP";
-export const LIVE_ASSET_URL = "https://cloudflare-ipfs.com/ipfs/Qma3Tyha3V5FWJpYyhN9FtFTMcitqFZdQ7fafzF4TPmzYH";
+export const EXAMPLE_ASSET_URL =
+  "https://cloudflare-ipfs.com/ipfs/QmXKiU24bL3t86aJNvFTGSbXdSA8CUFJM223MdaKqG9LfP";
 export const EXAMPLE_PUBLIC_KEY_JWK = {
   kty: "EC",
   crv: "P-256",
   x: "zRR-XGsCp12Vvbgui4DD6O6cqmhfPuXMhi1OxPl8760",
   y: "Iv5SU6FuW-TRYh5_GOrJlcV_gpF_GpFQhCOD8LSk3T0",
 };
-
+export const EXAMPLE_TOBESIGNED_MAX = 314;
 export const EXAMPLE_WASM_FILE = "nzcp_example.wasm";
 export const EXAMPLE_ZKEY_FILE = "nzcp_example_final.zkey";
 
-export const USE_REAL_PROOF = false;
-export const NETWORK = "rinkeby";
+export const LIVE_ASSET_URL =
+  "https://cloudflare-ipfs.com/ipfs/Qma3Tyha3V5FWJpYyhN9FtFTMcitqFZdQ7fafzF4TPmzYH";
+export const LIVE_PUBLIC_KEY_JWK = {
+  kty: "EC",
+  crv: "P-256",
+  x: "DQCKJusqMsT0u7CjpmhjVGkHln3A3fS-ayeH4Nu52tc",
+  y: "lxgWzsLtVI8fqZmTPPo9nZ-kzGs7w7XO8-rUU68OxmI",
+};
+export const LIVE_TOBESIGNED_MAX = 351;
+export const LIVE_WASM_FILE = "nzcp_live.wasm";
+export const LIVE_ZKEY_FILE = "nzcp_live_final.zkey";
